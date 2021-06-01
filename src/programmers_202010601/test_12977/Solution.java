3f6f4d3be1d6fb88a3cf12f38b4c79817fa10643
ngle-hyeyeong <sun_zero2@naver.com>
2021-06-02 01:59:33 +0900

    programmers 12977 수정

--- src/programmers_202010601/test_12977/Solution.java
+++ src/programmers_202010601/test_12977/Solution.java
@@ -1,24 +1,17 @@
 package programmers_202010601.test_12977;
 
-import java.util.ArrayList;
-
 public class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        ArrayList<Integer> results = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i != j) {
-                    for (int k = 0; k < nums.length; k++) {
-                        if (j != k && i != k) {
-                            int sum = nums[i] + nums[j] + nums[k];
-                            if (isPrime(sum) && !results.contains(sum)) {
-                                answer++;
-                                results.add(sum);
-                                System.out.println("[" + nums[i] + "," + nums[j] + "," + nums[k] + "]를 이용해서 " + sum + "을 만들 수 있습니다.");
-                            }
-                        }
+        int size = nums.length;
+        for (int i = 0; i < size - 2; i++) {
+            for (int j = i + 1; j < size - 1; j++) {
+                for (int k = j + 1; k < size; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) {
+                        answer++;
+                        System.out.println("[" + nums[i] + "," + nums[j] + "," + nums[k] + "]를 이용해서 " + sum + "을 만들 수 있습니다.");
                     }
                 }
             }
@@ -30,8 +23,8 @@ public class Solution {
     public static boolean isPrime(int num) {
         int end = (int) Math.sqrt(num);
 
-        for(int i = 2; i <= end; i++) {
-            if(num % i == 0) {
+        for (int i = 2; i <= end; i++) {
+            if (num % i == 0) {
                 return false;
             }
         }
